ea14d79f792110da750023dd5cfab4db5e86ca77
Nick Jason Hagen <nickjasonhagen@gmail.com>
2025-10-09 23:11:20 +0200

    setup update

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #nscript installer (compiling from source)
-
+echo copying this dir to ~/nscript, this will be your nscript shared envoirement!
+cp -r ./ ~/nscript && cd ~/nscript
 echo installing rust
-#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 cd ./setup/nscript && RUSTFLAGS='-C opt-level=3' cargo build --release
 mkdir ~/.local/bin
 echo copying the executable binary to ~/.local/bin/nscript 
